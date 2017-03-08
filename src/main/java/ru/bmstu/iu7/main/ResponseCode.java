c14f750c17b278373f4e6a2e3e0ffd3f460b2206
rashemihmih <mihmih96@gmail.com>
2017-03-09 00:19:21 +0300

    translated response messages into Russian

--- src/main/java/ru/bmstu/iu7/main/ResponseCode.java
+++ src/main/java/ru/bmstu/iu7/main/ResponseCode.java
@@ -2,11 +2,11 @@ package ru.bmstu.iu7.main;
 
 public enum  ResponseCode {
     OK(0, "OK"),
-    PARAMETER_MISSING(1, "Required parameter is missing"),
-    AUTH_ERROR(2, "Authorization error"),
-    DUPLICATE_ENTRY(3, "Duplicate entry"),
-    ENTRY_NOT_FOUND(4, "Entry not found"),
-    DB_ERROR(8, "Error querying database");
+    PARAMETER_MISSING(1, "В запросе не достаточно параметров"),
+    AUTH_ERROR(2, "Ошибка авторизации"),
+    DUPLICATE_ENTRY(3, "Запись уже существует"),
+    ENTRY_NOT_FOUND(4, "Запись не найдена"),
+    DB_ERROR(8, "Ошибка при работе с базой данных");
 
     private int code;
     private String message;
