6487b8adb928c812c9b610fa386da722edcf133c
rashemihmih <mihmih96@gmail.com>
2017-10-01 05:58:57 +0300

    optimize imports

--- src/test/java/ru/bmstu/iu7/AdminDaoTest.java
+++ src/test/java/ru/bmstu/iu7/AdminDaoTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.bmstu.iu7.dao.admin.Admin;
 import ru.bmstu.iu7.dao.admin.AdminDao;
